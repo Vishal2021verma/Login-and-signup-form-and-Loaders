2de1a057a4f1d32dd64b38315a7a9ca6e78c6492
Vishal Verma <89707084+Vishal2021verma@users.noreply.github.com>
2021-10-07 21:37:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openlogin(){
+  document.getElementById('id101').style.display = "block";
+}
+function opensignup(){
+  document.getElementById('id102').style.display = "block";
+}
+
+function closelogin(){
+  document.getElementById('id101').style.display = "none";
+}
+function closesignup(){
+  document.getElementById('id102').style.display = "none";
+}
